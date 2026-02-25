0abd1f038bcfdad4661a38631bd320b6c48cbad8
Sebastian Arnache Cantillo <sarnachecantillo@gmail.com>
2026-02-25 18:11:00 -0500

    intento 1

---
+++ buenas.py
@@ -0,0 +1,27 @@
+cantidad=int(input("ingresar la cantidad de personas que se desean registrar"))
+lista=[]
+def aprob():
+    if edad>=15 and conocimiento.lower=="si":
+     print("puede participar en el taller")
+    else:
+       print("no puede participar")
+
+
+for i in range(cantidad):
+    nombre=input("favor ingresar su nombre:  ")
+    edad=int(input("favor agregar su edad:  "))
+    if edad<=0:
+        print("se a detectado un error en la edad")
+        print("la edad no puede ser igual o menor a 0")
+        continue
+    
+    conocimiento=input("¿tiene el usuario conocimiento basico de computo")
+    aprob()
+    
+    dicc={"nombre":nombre,
+          "edad":edad,
+          "aprobado":aprob}
+    lista.append(dicc)
+print(f"el historial es el siguiente")
+for i in lista:
+   print(f"{i["nombre"]}---{i["edad"]}--{i["aprobado"]}")
